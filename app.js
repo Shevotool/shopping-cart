709df60e9e867b6ba95c04e37243da216ef7f03d
Andrés González <89161075+Shevotool@users.noreply.github.com>
2022-10-17 02:44:15 -0600

    Segundo Commit

--- app.js
+++ app.js
@@ -251,4 +251,50 @@ document.addEventListener("DOMContentLoaded", () => {
       ui.cartLogic();
     });
 });
-/* }); */
+/* ******** Dark theme ******** */
+function darkTheme(btn, classDark) {
+  const $themeBtn = document.querySelector(btn),
+    $selectors = document.querySelectorAll("[data-dark]");
+
+  //console.log($selectors);
+
+  let moon = "🌙";
+  let sun = "☀️";
+
+  const lightMode = () => {
+    $selectors.forEach((el) => el.classList.remove(classDark));
+    $themeBtn.textContent = moon;
+    localStorage.setItem("theme", "light");
+  };
+
+  const darkMode = () => {
+    $selectors.forEach((el) => el.classList.add(classDark));
+    $themeBtn.textContent = sun;
+    localStorage.setItem("theme", "dark");
+  };
+
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      console.log($themeBtn.textContent);
+      if ($themeBtn.textContent === moon) {
+        darkMode();
+      } else {
+        lightMode();
+      }
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", (e) => {
+    //console.log(ls.getItem("theme"));
+    if (localStorage.getItem("theme") === null) ls.setItem("theme", "light");
+    if (localStorage.getItem("theme") === "light") {
+      lightMode();
+    }
+    if (localStorage.getItem("theme") === "dark") {
+      darkMode();
+    }
+  });
+}
+darkTheme(".dark-theme-btn", "dark-mode");
+
+/* ******** End Dark theme ******** */
